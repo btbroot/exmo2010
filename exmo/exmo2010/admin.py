4313c55807d2d54f90ae2e3b6b77b5b7542442ed
Al Nikolov <al@home.toor.spb.ru>
2010-02-22 21:41:17 +0300

    initial parameters and criterias. some fixes to the model

--- exmo/exmo2010/admin.py
+++ exmo/exmo2010/admin.py
@@ -1,11 +1,13 @@
-from exmo.exmo2010.models import Score, Organization, Parameter, Quality
+from exmo.exmo2010.models import Score, Organization, Category, Subcategory, Parameter, Criteria
 from django.contrib import admin
 
 class ScoreAdmin(admin.ModelAdmin):
-  list_filter   = ('organization', 'author')
-  list_display  = ('organization', 'parameter', 'quality', 'score', 'author')
+  #list_filter   = ('organization', 'author')
+  list_display  = ('organization', 'parameter', 'criteria')
 
 admin.site.register(Score, ScoreAdmin)
 admin.site.register(Organization)
+admin.site.register(Category)
+admin.site.register(Subcategory)
 admin.site.register(Parameter)
-admin.site.register(Quality)
+admin.site.register(Criteria)
--- exmo/exmo2010/models.py
+++ exmo/exmo2010/models.py
@@ -3,23 +3,62 @@ from django.contrib.auth.models import User
 
 class Organization(models.Model):
   name         = models.CharField(max_length = 200, unique = True)
-  url          = models.CharField(max_length = 200)
+  url          = models.CharField(max_length = 200, unique = True)
 
   def __unicode__(self):
     return self.name
 
 
-class Parameter(models.Model):
+class Category(models.Model):
+  code         = models.IntegerField(unique = True)
   name         = models.CharField(max_length = 200, unique = True)
-  shortName    = models.CharField(max_length = 10)
+
+  def __unicode__(self):
+    return '%d. %s' % (self.code, self.name)
+
+  class Meta:
+    ordering = ('code',)
+
+
+class Subcategory(models.Model):
+  code         = models.IntegerField()
+  name         = models.CharField(max_length = 200)
+  group        = models.ForeignKey(Category)
+
+  def __unicode__(self):
+    return '%d.%d. %s' % (self.group.code, self.code, self.name)
+
+  class Meta:
+    unique_together = (
+      ('name', 'group'),
+      ('code', 'group'),
+    )
+    ordering = ('group__code', 'code')
+
+
+class Parameter(models.Model):
+  code         = models.IntegerField()
+  name         = models.CharField(max_length = 200)
+  description  = models.CharField(max_length = 1024)
+  group        = models.ForeignKey(Subcategory)
   weight       = models.IntegerField()
 
   def __unicode__(self):
-    return '%s. %s' % (self.shortName, self.name)
+    return '%d.%d.%d. %s' % (self.group.group.code, self.group.code, self.code, self.name)
+
+  class Meta:
+    unique_together = (
+      ('name', 'group'),
+      ('code', 'group'),
+    )
+    ordering = ('group__group__code', 'group__code', 'code')
 
 
-class Quality(models.Model):
-  name         = models.CharField(max_length = 200, unique = True)
+class Criteria(models.Model):
+  code         = models.CharField(max_length = 2, unique = True)
+  name         = models.CharField(max_length = 20, unique = True)
+  minimum      = models.IntegerField()
+  maximum      = models.IntegerField()
 
   def __unicode__(self):
     return self.name
@@ -28,11 +67,11 @@ class Quality(models.Model):
 class Score(models.Model):
   organization = models.ForeignKey(Organization)
   parameter    = models.ForeignKey(Parameter)
-  quality      = models.ForeignKey(Quality)
+  criteria     = models.ForeignKey(Criteria)
   score        = models.IntegerField(null = True, blank = True)
   author       = models.ForeignKey(User, null = True, blank = True)
-  
+
   class Meta:
     unique_together = (
-      ('organization', 'parameter', 'quality', 'author'),
+      ('organization', 'parameter', 'criteria', 'author'),
     )
