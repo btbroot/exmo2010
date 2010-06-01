ca852cd34d52bf9daa08dcf75940e60bf9245f9f
Boris Savelev <boris@altlinux.org>
2010-06-01 17:25:01 +0400

    rewrite admin urls for revertion works (see comments in http://code.google.com/p/django-reversion/wiki/GettingStarted)

--- exmo/urls.py
+++ exmo/urls.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-  (r'^admin/(.*)', admin.site.root),
+  #(r'^admin/(.*)', admin.site.root),
+  (r'^admin/', include(admin.site.urls)),
   (r'^exmo2010/', include('exmo.exmo2010.urls')),
 )
 
