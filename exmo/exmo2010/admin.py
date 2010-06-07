0453305317dcc60700798998bcecb765eb71810f
Boris Savelev <boris@altlinux.org>
2010-06-07 12:16:41 +0400

    admin: override change_view for ScoreAdmin; put example for filter in list

--- exmo/exmo2010/admin.py
+++ exmo/exmo2010/admin.py
@@ -2,8 +2,70 @@ import exmo.exmo2010.models
 from django.contrib import admin
 from reversion.admin import VersionAdmin
 
+
+from django.shortcuts import get_object_or_404
 class ScoreAdmin(VersionAdmin):
-    pass
+
+    radio_fields = {
+	"found": admin.HORIZONTAL,
+	"complete": admin.HORIZONTAL,
+	"topical": admin.HORIZONTAL,
+	"accessible": admin.HORIZONTAL,
+    }
+
+    readonly_fields = ('task', 'parameter',)
+
+    def queryset(self, request):
+        if request.user.is_superuser:
+            qs = self.model._default_manager.get_query_set()
+        else:
+            qs = self.model._default_manager.get_query_set().filter(user = request.user)
+
+        ordering = self.ordering or ()
+        if ordering:
+            qs = qs.order_by(*ordering)
+        return qs
+
+
+    # A template for a very customized change view:
+    change_form_template = 'admin/score_change_form.html'
+
+    def change_view(self, request, object_id, extra_context=None):
+	parameter = get_object_or_404(exmo.exmo2010.models.Score, pk=object_id).parameter
+
+	main_fieldset = (
+	  ('main', {
+	    'fields': ('task','parameter','found',),
+	  })
+	)
+	comment_fieldset = (
+	  ('comment', {
+	    'classes': ('collapse',),
+	    'fields': ('comment',)
+	  })
+	)
+	ct = at = tt = None
+	if parameter.completeRequired:
+          ct = ('complete', {'fields': ('complete', 'completeComment',)})
+
+	if parameter.topicalRequired:
+          tt = ('topical', {
+            'fields': ('topical', 'topicalComment',)
+          })
+	if parameter.accessibleRequired:
+          at = ('accessible', {
+            'fields': ('accessible', 'accessibleComment',)
+          })
+        if ct and tt and at:
+          self.fieldsets = main_fieldset, ct, tt, at, comment_fieldset
+        elif ct and tt:
+          self.fieldsets = main_fieldset, ct, tt, comment_fieldset
+        elif ct and at:
+          self.fieldsets = main_fieldset, ct, at, comment_fieldset
+        elif at and tt:
+          self.fieldsets = main_fieldset, tt, at, comment_fieldset
+        return super(ScoreAdmin, self).change_view(request, object_id,
+            extra_context={ 'parameter': parameter })
 
 class OrganizationTypeAdmin(admin.ModelAdmin):
   list_display = ('pk', 'name')
