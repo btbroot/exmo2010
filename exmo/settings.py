25702ade27e34e6f34a4098532cb50b1b8fa2cb7
Boris Savelev <boris@altlinux.org>
2010-06-07 19:34:16 +0400

    enable reversion middleware

--- exmo/settings.py
+++ exmo/settings.py
@@ -64,6 +64,8 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.middleware.transaction.TransactionMiddleware',
+    'reversion.middleware.RevisionMiddleware',
 #    'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
