fea3346a68f2908ca66e6fde2ac5d4f6b5fc9439
Boris Savelev <boris@altlinux.org>
2010-06-07 19:35:11 +0400

    import reversion for models

--- exmo/exmo2010/models.py
+++ exmo/exmo2010/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+import reversion
+
+
 
 class OrganizationType(models.Model):
   name         = models.CharField(max_length = 200, unique = True)
