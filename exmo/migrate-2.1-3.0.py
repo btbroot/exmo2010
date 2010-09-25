5edf87a8c0c2322ff11a1df52bdb9ce3c3d544c1
Al Nikolov <root@toor.pp.ru>
2010-09-26 02:39:08 +0300

    Migration script created, really #74

--- exmo/migrate-2.1-3.0.py
+++ exmo/migrate-2.1-3.0.py
@@ -15,7 +15,18 @@ for obj in old_data:
       })
   # Fix model Task
   elif obj['model'] == 'exmo2010.task':
-    obj['fields']['monitoring'] = [ obj['pk'] for obj in old_data if obj['model'] == 'exmo2010.organizationtype' ][0]
+    obj['fields']['monitoring'] = [
+      org['fields']['type']
+        for org in old_data
+          if org['model'] == 'exmo2010.organization' and
+             org['pk']    == obj['fields']['organization']
+    ][0]
+    del obj['fields']['organization']
+    new_data.append(obj)
+  # Fix model Parameter
+  elif obj['model'] == 'exmo2010.parameter':
+    obj['fields']['monitoring'] = obj['fields']['organizationtype']
+    del obj['fields']['organizationtype']
     new_data.append(obj)
   else:
     new_data.append(obj)
