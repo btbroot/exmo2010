8cd97499b41f26d4f85ebd9b61060b039a287c42
Al Nikolov <root@toor.pp.ru>
2010-09-26 14:08:43 +0300

    Really migrating data. 50% tested! #74

--- exmo/migrate-2.1-3.0.py
+++ exmo/migrate-2.1-3.0.py
@@ -21,14 +21,18 @@ for obj in old_data:
           if org['model'] == 'exmo2010.organization' and
              org['pk']    == obj['fields']['organization']
     ][0]
-    del obj['fields']['organization']
-    new_data.append(obj)
+    if obj['fields']['open']:
+      obj['fields']['status'] = 0
+    else:
+      obj['fields']['status'] = 1
+    del obj['fields']['open']
   # Fix model Parameter
   elif obj['model'] == 'exmo2010.parameter':
-    obj['fields']['monitoring'] = obj['fields']['organizationtype']
-    del obj['fields']['organizationtype']
-    new_data.append(obj)
-  else:
-    new_data.append(obj)
+    obj['fields']['monitoring'] = obj['fields']['organizationType']
+    del obj['fields']['organizationType']
+  # Fix model Organization
+  elif obj['model'] == 'exmo2010.organization':
+    obj['fields']['keyname'] = str(obj['pk'])
+  new_data.append(obj)
 
 simplejson.dump(new_data, open('data-3.0.json', 'w'))
