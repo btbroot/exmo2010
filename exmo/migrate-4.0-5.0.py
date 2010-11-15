706003a02ae4cd74d000421f1b1aede12a6c0269
Al Nikolov <root@toor.pp.ru>
2010-11-15 23:45:00 +0200

    Migration to 5.0 #187

---
+++ exmo/migrate-4.0-5.0.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import simplejson
+
+old_data = simplejson.load(open('data-4.0.json'))
+new_data = []
+
+for obj in old_data:
+  # Delete model ApprovedTask
+  if obj['model'] == 'exmo2010.approvedtask':
+    continue
+  # Fix model Task
+  elif obj['model'] == 'exmo2010.task':
+    if [ o for o in old_data
+           if o['model'] == 'exmo2010.approvedtask'
+           and o['fields']['task'] == obj['pk'] ]:
+      obj['fields']['status'] = 2
+    elif obj['fields']['closed']:
+      obj['fields']['status'] = 1
+    else:
+      obj['fields']['status'] = 0
+  new_data.append(obj)
+
+simplejson.dump(new_data, open('data-5.0.json', 'w'))
