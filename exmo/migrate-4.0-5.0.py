0f29b055cd03eba8d2d6bb67a5bf0ff2e5b1f628
Boris Savelev <boris@altlinux.org>
2010-11-16 11:43:35 +0300

    remove unneeded field closed from Task model

--- exmo/migrate-4.0-5.0.py
+++ exmo/migrate-4.0-5.0.py
@@ -19,6 +19,7 @@ for obj in old_data:
       obj['fields']['status'] = 1
     else:
       obj['fields']['status'] = 0
+    del obj['fields']['closed']
   new_data.append(obj)
 
 simplejson.dump(new_data, open('data-5.0.json', 'w'))
