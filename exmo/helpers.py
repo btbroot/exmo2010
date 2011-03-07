7e4ee33a9ecf92e81a0c27e61b4aef167f2efe17
Boris Savelev <boris.savelev@gmail.com>
2011-03-07 16:46:11 +0300

    disable signals for syncdb too

--- exmo/helpers.py
+++ exmo/helpers.py
@@ -12,7 +12,7 @@ def disable_for_loaddata(signal_handler):
     @wraps(signal_handler)
     def wrapper(*args, **kwargs):
         for fr in inspect.stack():
-            if inspect.getmodulename(fr[1]) == 'loaddata':
+            if inspect.getmodulename(fr[1]) in  ('loaddata','syncdb'):
                 return
         signal_handler(*args, **kwargs)
     return wrapper
