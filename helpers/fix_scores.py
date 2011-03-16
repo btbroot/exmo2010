8b0b5a6612aad3a07f7cba5615cb29a4418ba9cc
Al Nikolov <root@toor.fi.eu.org>
2011-03-16 19:16:27 +0200

    More copyright fixes. Incorporating django403 files by GPL-3.

--- helpers/fix_scores.py
+++ helpers/fix_scores.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python
+# This file is part of EXMO2010 software.
+# Copyright 2010, 2011 Al Nikolov
+# Copyright 2010, 2011 Institute for Information Freedom Development
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 '''
 Fix scores for error in validation (sa: #189).
