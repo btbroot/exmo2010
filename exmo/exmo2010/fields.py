a2e35eb04154b581c7b33cfeba021bd9afc41ed9
Boris Savelev <boris@altlinux.org>
2011-05-16 16:39:24 +0400

    remove validation from TagField

--- exmo/exmo2010/fields.py
+++ exmo/exmo2010/fields.py
@@ -1,21 +1,20 @@
 """
 A custom Model Field for tagging.
 """
-from tagging.fields import TagField
+from tagging.fields import TagField as TagField_orig
+from django.core import validators
 
-class TagField(TagField):
+class TagField(TagField_orig):
     """
     A "special" character field that actually works as a relationship to tags
     "under the hood". This exposes a space-separated string of tags, but does
     the splitting/reordering/etc. under the hood.
     """
     def __init__(self, *args, **kwargs):
+        kwargs['max_length'] = kwargs.get('max_length', 255)
         kwargs['blank'] = kwargs.get('blank', True)
-        if kwargs.has_key('create_synonyms'):
-            self.create_synonyms = kwargs.pop('create_synonyms')
-        else:
-            self.create_synonyms = None
         super(TagField, self).__init__(*args, **kwargs)
+        self.validators = [] #hack. but nothing to validate
 
     def get_internal_type(self):
         return 'TextField'
