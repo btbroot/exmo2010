b2163f4eaf34b9709c5b87780e6894a14f89e8eb
Boris Savelev <boris.savelev@gmail.com>
2011-12-12 14:44:43 +0400

    inline UserProfile into User admin

--- exmo/exmo2010/admin.py
+++ exmo/exmo2010/admin.py
@@ -65,6 +65,40 @@ class UserProfileAdmin(admin.ModelAdmin):
             "all": ("exmo2010/selector.css",)
         }
 
+
+
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+
+from exmo.exmo2010.models import UserProfile
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    formfield_overrides = {
+        models.ManyToManyField: {
+            'widget': admin.widgets.FilteredSelectMultiple('',
+                                                           is_stacked=False)
+        },
+    }
+    class Media:
+        css = {
+            "all": ("exmo2010/selector.css",)
+        }
+
+
+class UserAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.ManyToManyField: {
+            'widget': admin.widgets.FilteredSelectMultiple('',
+                                                           is_stacked=False)
+        },
+    }
+    inlines = [UserProfileInline]
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
 admin.site.register(exmo.exmo2010.models.Organization, OrganizationAdmin)
 admin.site.register(exmo.exmo2010.models.Parameter, ParameterAdmin)
 admin.site.register(exmo.exmo2010.models.Score, ScoreAdmin)
@@ -72,5 +106,4 @@ admin.site.register(exmo.exmo2010.models.Task, TaskAdmin)
 admin.site.register(exmo.exmo2010.models.Monitoring, MonitoringAdmin)
 admin.site.register(exmo.exmo2010.models.MonitoringStatus)
 admin.site.register(exmo.exmo2010.models.Claim)
-admin.site.register(exmo.exmo2010.models.UserProfile, UserProfileAdmin)
 admin.site.register(exmo.exmo2010.models.OpennessExpression)
