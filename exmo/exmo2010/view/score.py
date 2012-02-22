229e60f58a5f3218543f269a3644511de4702d50
Boris Savelev <boris.savelev@gmail.com>
2012-02-22 13:45:44 +0400

    #296

--- exmo/exmo2010/view/score.py
+++ exmo/exmo2010/view/score.py
@@ -164,6 +164,7 @@ def score_list_by_task(request, task_id, report=None):
           (_('HTML'), None, None, None, None),
           (_('Document'), None, None, None, None),
           (_('Image'), None, None, None, None),
+          (_('Weight'), None, None, None, None),
         ),
         queryset=queryset,
         template_name='exmo2010/score_list.html',
