5371971b84b422235f103116d8dadd09bc022cdb
Boris Savelev <boris.savelev@gmail.com>
2012-05-28 19:43:17 +0400

    add custom menu for dashboard

---
+++ exmo/user_dashboard/menu.py
@@ -0,0 +1,29 @@
+"""
+This file was generated with the custommenu management command, it contains
+the classes for the admin menu, you can customize this class as you want.
+
+To activate your custom menu add the following to your settings.py::
+    ADMIN_TOOLS_MENU = 'exmo.menu.CustomMenu'
+"""
+
+from django.core.urlresolvers import reverse
+from django.utils.translation import ugettext_lazy as _
+
+from admin_tools.menu import items, Menu
+
+
+class CustomMenu(Menu):
+    """
+    Custom Menu for exmo admin site.
+    """
+    def __init__(self, **kwargs):
+        Menu.__init__(self, **kwargs)
+        self.children += [
+            items.MenuItem(_('Dashboard'), reverse('user_dashboard:index')),
+        ]
+
+    def init_with_context(self, context):
+        """
+        Use this method if you need to access the request context.
+        """
+        return super(CustomMenu, self).init_with_context(context)
