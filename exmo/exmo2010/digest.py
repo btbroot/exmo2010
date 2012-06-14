67fbc016c88c62e90458e697d4379ff882d896e1
Boris Savelev <boris.savelev@gmail.com>
2012-06-14 13:44:23 +0400

    add comment digest class and templates

---
+++ exmo/exmo2010/digest.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from digest_email.digest import DigestSend
+from digest_email import models as digest_models
+from datetime import datetime
+from django.contrib.comments.models import Comment
+from exmo2010.models import Score
+
+class ScoreCommentDigest(DigestSend):
+
+    def get_content(self, user, timestampt = datetime.now()):
+        "Собираем комментарии для отправления с момента последней отправки дайджеста по timestamp"
+
+        if user.userprofile.is_expertA or user.userprofile.is_manager_expertB:
+            score_pk = Score.objects.all()
+        elif user.userprofile.is_expertB:
+            score_pk = Score.objects.filter(task__user = user)
+        elif user.userprofile.is_organization:
+            score_pk = Score.objects.filter(task__orgazation__in = user.userprofile.organization)
+
+        last_digest_date = self.digest.get_last(user)
+        qs = Comment.objects.filter(
+            content_type__model = 'score',
+            object_pk__in = score_pk.values_list('id', flat=True),
+        ).order_by('submit_date')
+
+        if last_digest_date:
+            qs = qs.filter(submit_date__gte = last_digest_date)
+        if not user.userprofile.notify_score_preference['self']:
+            qs = qs.exclude(user = user)
+        return qs
+
+
+class ScoreCommentDigest(DigestSend):
+
+    def get_content(self, user, timestampt = datetime.now()):
+        "Собираем комментарии для отправления с момента последней отправки дайджеста по timestamp"
---
+++ exmo/exmo2010/templatetags/settings_value.py
@@ -0,0 +1,12 @@
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+# settings value
+@register.simple_tag
+def settings_value(name):
+    try:
+        return settings.__getattr__(name)
+    except AttributeError:
+        return ""
