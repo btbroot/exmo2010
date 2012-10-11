97bc51580e7fa172b6ac837944b81e005099e615
Alexandr Mishurov <amishurov@svobodainfo.org>
2012-10-11 16:23:14 +0400

    Bug #845 Создание задачи со страницы организации
    Feature:   В любом цикле мониторинга, если зайти в Органы/Организации? (например,  http://office.svobodainfo.org:8011/exmo2010/monitoring/35/organizations/).
              Выбрать любую организацию. Зайти в нее ( http://office.svobodainfo.org:8011/exmo2010/monitoring/35/organization/5687/tasks/)
              И нажать на "Добавить задачу"
              Выбрать имя эксперта в выпадающем списке и нажать на кнопку "Отправить"
    
              Задача не создается, со страницей тоже ничего не происходит. Т.е. нет никакой реакции на нажатие кнопки.
              Кнопка "Сброс" так же не работает.
    Scenario: вывел полностью форму в темплейте (без скрытых полей), оказалось, что по-умолчанию не заполняется поле "organization" у формы (валидатор выдает ошибку). Заполнил форму переменной из вью во время GET-запроса (т.е. до нажатия кнопки "Submit"). Работает и создание, и ресет.

--- exmo/exmo2010/view/task.py
+++ exmo/exmo2010/view/task.py
@@ -308,10 +308,11 @@ def task_add(request, monitoring_id, organization_id=None):
         organization = None
         redirect = '%s?%s' % (reverse('exmo2010:tasks_by_monitoring', args=[monitoring.pk]), request.GET.urlencode())
         title = _('Add new task for %s') % monitoring
+
     redirect = redirect.replace("%","%%")
     if request.user.has_perm('exmo2010.admin_monitoring', monitoring):
         if request.method == 'GET':
-            form = TaskForm(monitoring=monitoring)
+            form = TaskForm(initial={'organization':organization}, monitoring=monitoring)
             return render_to_response(
                 'exmo2010/task_form.html',
                 {
@@ -334,7 +335,7 @@ def task_add(request, monitoring_id, organization_id=None):
                     'monitoring': monitoring,
                     'organization': organization,
                     'title': title,
-                    'form': form
+                    'form': form,
                 },
                 context_instance=RequestContext(request),
             )
