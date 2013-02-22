ad55a1450e988ccc8fdca180d9c2aea5eada23fd
Alexandr Mishurov <amishurov@svobodainfo.org>
2013-02-22 12:12:26 +0400

    Bug #976 Нарушения лицензии
    Feature:  Для закрытия всех вопросов с нарушениями лицензии AGPL, доделать следующее:
    
                 Добавить лицензию для файла selector.css
                 Удалить из проекта exmo/exmo2010/static/exmo2010/socialauth/VK.png, exmo/exmo2010/static/exmo2010/socialauth/f_logo.png за ненадобностью, удалить упоминание о них в COPYING
                 Сменить иконку для календаря на  http://png.findicons.com/files/icons/2181/34al_volume_3_2_se/24/001_44.png
    
             (указать в COPYING ссылку на сайт  http://www.icojam.com/blog/?p=139, там же текст лицензии)
             Старую иконку календаря и ее упоминание в COPYING - удалить.
    
    Scenario: В файле  selector.css уже была лицензия, остальное исправил, дополнительно добавил в новые шаблоны лицензии, куда забывал добавлять.

--- exmo/exmo2010/migrations/0009_auto__add_questionnaire__add_qanswer__add_unique_qanswer_task_question.py
+++ exmo/exmo2010/migrations/0009_auto__add_questionnaire__add_qanswer__add_unique_qanswer_task_question.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # This file is part of EXMO2010 software.
 # Copyright 2010, 2011 Al Nikolov
-# Copyright 2010, 2011, 2012 Institute for Information Freedom Development
+# Copyright 2010, 2011, 2012, 2013 Institute for Information Freedom Development
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
