a9ac51d09affd240ec03144603cf2ae07262de60
Boris Savelev <boris.savelev@gmail.com>
2013-02-22 10:37:43 +0400

    update copy_monitoring for new models

--- helpers/copy_monitoring.py
+++ helpers/copy_monitoring.py
@@ -27,53 +27,78 @@ import sys
 os.environ['DJANGO_SETTINGS_MODULE'] = "exmo.settings"
 path = "%s/.." % os.path.realpath(os.path.dirname(__file__))
 sys.path.append(os.path.realpath(path))
+path = "%s/../exmo" % os.path.realpath(os.path.dirname(__file__))
+sys.path.append(os.path.realpath(path))
 
+from django.contrib.comments.models import Comment
 import exmo2010.models as em
+from copy import deepcopy
+
+NEED_COMMENT=True
+NEED_COMMENT=False
+
 def copy_monitoring(monitoring_pk):
     m_src = em.Monitoring.objects.get(pk = monitoring_pk)
     #create monitoring
     m_name = '%s_new' % m_src.name
-    m = em.Monitoring(name = m_name, type = m_src.type)
+    m = deepcopy(m_src)
+    m.pk = None
+    m.name = m_name
+    m.status = em.Monitoring.MONITORING_RATE
     m.save()
+    print m.pk
 
-    #parameters
-    params = em.Parameter.objects.filter(monitoring = m_src)
-    for p in params:
-        w = em.ParameterMonitoringProperty.objects.get(monitoring = m_src, parameter = p).weight
-        pmp = em.ParameterMonitoringProperty(monitoring = m, parameter = p, weight = w)
-        pmp.save()
+    organization_map = {}
+    parameter_map = {}
 
     #tasks
-    tasks = em.Task.objects.filter(monitoring = m_src)
+    tasks = em.Task.objects.filter(organization__monitoring = m_src, status= em.Task.TASK_APPROVED)
     for t in tasks:
-        task = em.Task(
-            user = t.user,
-            organization = t.organization,
-            monitoring = m,
-            status = t.status,
-            )
+        org = deepcopy(t.organization)
+        org.pk = None
+        org.monitoring = m
+        org.inv_code = em.generate_inv_code(6)
+        org.save()
+        organization_map[t.organization.pk] = org.pk
+        task = deepcopy(t)
+        task.pk = None
+        task.organization = org
         task.save()
         scores = em.Score.objects.filter(task = t)
         for s in scores:
-            score = em.Score(
-                task = task,
-                parameter = s.parameter,
-                found = s.found,
-                complete = s.complete,
-                completeComment = s.completeComment,
-                topical = s.topical,
-                topicalComment = s.topicalComment,
-                accessible = s.accessible,
-                accessibleComment = s.accessibleComment,
-                hypertext = s.hypertext,
-                hypertextComment = s.hypertextComment,
-                document = s.document,
-                documentComment = s.documentComment,
-                image = s.image,
-                imageComment = s.imageComment,
-                comment = s.comment,
-                )
+            if em.Parameter.objects.filter(
+                monitoring = m,
+                code = s.parameter.code,
+            ).exists():
+                param = em.Parameter.objects.get(
+                    monitoring = m,
+                    code = s.parameter.code)
+            else:
+                param = deepcopy(s.parameter)
+                param.pk = None
+                param.monitoring = m
+                param.save()
+                param.exclude.clear()
+                parameter_map[s.parameter.pk] = param.pk
+            score = deepcopy(s)
+            score.pk = None
+            score.task = task
+            score.parameter = param
             score.save()
 
+            if NEED_COMMENT:
+                comments = Comment.objects.for_model(em.Score).filter(object_pk=s.pk)
+                for c in comments:
+                    comment = deepcopy(c)
+                    comment.pk = None
+                    comment.object_pk = score.pk
+                    comment.save()
 
-copy_monitoring(1)
+    for param_pk, new_param_pk in parameter_map.iteritems():
+        param = em.Parameter.objects.get(pk=param_pk)
+        new_param = em.Parameter.objects.get(pk=new_param_pk)
+        for org_pk in param.exclude.values_list('pk', flat=True):
+            if organization_map.has_key(org_pk):
+                new_param.exclude.add(em.Organization.objects.get(pk=organization_map[org_pk]))
+
+copy_monitoring(50)
