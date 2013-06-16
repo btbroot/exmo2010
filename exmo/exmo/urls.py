ced1251daa57dd709095eb92131684d867669542
vintorez <okulichev@rambler.ru>
2013-06-16 11:22:24 +0400

    Bug #1467 Падение на главной странице 2
    Ошибка 502 при заходе на главную страницу.

--- exmo/exmo/urls.py
+++ exmo/exmo/urls.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # This file is part of EXMO2010 software.
-# Copyright 2010, 2011 Al Nikolov
+# Copyright 2010, 2011, 2013 Al Nikolov
 # Copyright 2010, 2011 non-profit partnership Institute of Information Freedom Development
 # Copyright 2012, 2013 Foundation "Institute for Information Freedom Development"
 #
@@ -24,7 +24,7 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponsePermanentRedirect
 
 from core import site as exmo
-from core.helpers import TemplateView
+from core.views import TemplateView
 admin.autodiscover()
 
 
