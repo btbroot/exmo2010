c932fd1fae5fe786f9546f977e5f9bec095abc72
vintorez <okulichev@rambler.ru>
2014-06-06 12:54:06 +0400

    Technical Debt #2100 Удалить не используемые темплейт-теги.
    Удалить темплейт-тег setting_value.
    Заменить first_element на фильтр шаблона.
    Объединить date_until и days_until.

--- exmo/core/templatetags/date_utils.py
+++ exmo/core/templatetags/date_utils.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # This file is part of EXMO2010 software.
-# Copyright 2010, 2011, 2013 Al Nikolov
+# Copyright 2010, 2011 Al Nikolov
 # Copyright 2010, 2011 non-profit partnership Institute of Information Freedom Development
-# Copyright 2012, 2013 Foundation "Institute for Information Freedom Development"
+# Copyright 2012-2014 Foundation "Institute for Information Freedom Development"
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -18,8 +18,8 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 from datetime import date, datetime
-
 from dateutil.rrule import rrule, DAILY, MO, TU, WE, TH, FR
+
 from django import template
 
 
@@ -27,20 +27,36 @@ register = template.Library()
 
 
 @register.filter(is_safe=False)
+def date_until(start, limit):
+    """
+    Прибавляет к начальной дате количество рабочих дней.
+
+    """
+    return _next_date_generator(start, limit)
+
+
+@register.filter(is_safe=False)
 def workdays_still_left(start, limit):
     """
     Количество рабочих дней, оставшихся на сегодня до исчерпания периода.
-    start: дата начала периода, datetime
-    limit: величина периода в рабднях, int/long
 
     """
-    if not isinstance(start, datetime):
-        return None
-    if not isinstance(limit, (int, long)):
+    next_date = _next_date_generator(start, limit)
+    result = (next_date.date() - date.today()).days
+
+    return result
+
+
+def _next_date_generator(start, limit):
+    """
+    :param start: дата начала периода, datetime
+    :param limit: величина периода в рабочих днях, int/long
+    :return: возвращает новую дату, datetime
+
+    """
+    if not isinstance(start, datetime) or not isinstance(limit, (int, long)):
         return None
     workdays_generator = rrule(DAILY, byweekday=(MO, TU, WE, TH, FR), dtstart=start)
     next_date = workdays_generator[limit]
-    today = date.today()
-    result = (next_date.date() - today).days
 
-    return result
+    return next_date
