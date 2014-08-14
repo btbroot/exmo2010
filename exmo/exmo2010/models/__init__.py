df9df521269d41c565d4b7d411a6d94509e30213
vintorez <okulichev@rambler.ru>
2014-08-14 15:33:10 +0400

    Technical Debt #2169 Заменить адрес почты и вынести настройки почты в livestttings.
    Заменить адрес почты с которого отправляются все письма системы на  system@infometer.org
    Вынести настройки почты (пароли и т.д.) в livesettings.

--- exmo/exmo2010/models/__init__.py
+++ exmo/exmo2010/models/__init__.py
@@ -17,9 +17,6 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-
-from exmo import config  # livesettings init
-
 from .claim import *
 from .clarification import *
 from .monitoring import *
