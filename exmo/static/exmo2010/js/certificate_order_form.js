e1756782b47641cbec8e37b87a943e63ef9bec7e
vintorez <okulichev@rambler.ru>
2014-10-14 19:47:34 +0400

    Bug #1962 Неправильная сортировка радиокнопок при нажатии Назад в Chrome.
    Если на странице заказа сертификата отсортировать поля в таблице как угодно (только не в той же последовательности, что была при загрузке страницы) и нажать кнопку "Заказать сертификат", попасть на вторую страницу, а после этого нажать кнопку "Назад" в браузере, то в списке рейтингов выбранной становится не та радиокнопка, которая была выбрана изначально.
    Если сортировку перед заказом сертификата не делать (или делать, но вернуть все в начальное состояние), то все работает корректно.
    Воспроизводится только в Chrome!
    При нажатии Назад Хром возвращает страницу с сортировкой по умолчанию, в то время как все остальные браузеры показывают страницу с той сортировкой, которая была у пользователя в момент перед нажатием кнопки "Заказать сертификат".
    
    При переходе назад, Chrome игнорирует изменения на странице, созданные javascript`ом, так как WebKit не имеет кэша для кнопок назад/вперед  https://code.google.com/p/chromium/issues/detail?id=2879
    
    Решено убрать JS-сортировку со страницы заказа сертификатов.

--- exmo/static/exmo2010/js/certificate_order_form.js
+++ exmo/static/exmo2010/js/certificate_order_form.js
@@ -2,6 +2,7 @@
 // Copyright 2010, 2011, 2013 Al Nikolov
 // Copyright 2010, 2011 non-profit partnership Institute of Information Freedom Development
 // Copyright 2012-2014 Foundation "Institute for Information Freedom Development"
+// Copyright 2014 IRSI LTD
 //
 //    This program is free software: you can redistribute it and/or modify
 //    it under the terms of the GNU Affero General Public License as
@@ -16,13 +17,11 @@
 //    You should have received a copy of the GNU Affero General Public License
 //    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
+
 $(document).ready(function() {
 
     /* placeholder */
     jQuery('input[placeholder]').placeholder();
-
-    /* Sortable table */
-    $("#rating-data").tablesorter({sortList: [[0, 1]]});
 
     /* Active tabs */
     var rating_type = $('div.tabs').data('rating_type');
